19a5b4c2e2dffc614eaa1026353fee5b7b099dcb
IMzhushang <zhushang1996@163.com>
2017-07-09 09:10:15 +0800

    Update ClassLoaderHelper.java

--- src/main/java/com/simpleweb/framework/helper/ClassLoaderHelper.java
+++ src/main/java/com/simpleweb/framework/helper/ClassLoaderHelper.java
@@ -54,7 +54,7 @@ public class ClassLoaderHelper {
 	 */
 	public static Set<Class<?>> getControllerClassSet() {
 		Set<Class<?>> sets = new HashSet<Class<?>>();
-
+                Set<Class<?>> sets3 = new HashSet<Class<?>>();
 		for (Class<?> clazz : SET_CLASS) {
 			// 判断一个类文件上是否有service注解
 			if (clazz.isAnnotationPresent(Controller.class)) {
